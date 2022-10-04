6547e5041f27992052bef886f0e29d7d7dfe72f1
Денис Джабаров <denis.smit72@gmail.com>
2022-10-04 08:57:54 +0500

    Прописал логику для работы с LocalStorage

---
+++ src/utils/TaskStorage.ts
@@ -0,0 +1,84 @@
+import {LocalKey, LocalStorage} from 'ts-localstorage';
+
+import {ITask} from '../store/reducers/tasks';
+
+const tasksKey = new LocalKey<ITask[]>('tasks', []);
+const lastIndexKey = new LocalKey<number>('last-index', 1);
+
+const getTasks = () => {
+  const tasks = LocalStorage.getItem(tasksKey);
+  
+  if (tasks) {
+    return tasks;
+  }
+  
+  return [];
+};
+
+const setTasks = (tasks: ITask[]) => {
+  LocalStorage.setItem(tasksKey, tasks);
+};
+
+const addTask = (task: ITask) => {
+  const tasks = getTasks();
+
+  tasks.push(task);
+
+  setTasks(tasks);
+};
+
+const editTask = (task: ITask) => {
+  const tasks = getTasks();
+  const taskIndex = tasks.findIndex(item => task.id === item.id);
+  
+  if (taskIndex === -1) {
+    return;
+  }
+  
+  tasks.splice(taskIndex, 1, task);
+  
+  setTasks(tasks);
+};
+
+const deleteTask = (taskId: number) => {
+  const tasks = getTasks();
+  const taskIndex = tasks.findIndex(item => taskId === item.id);
+
+  if (taskIndex === -1) {
+    return;
+  }
+
+  tasks.splice(taskIndex, 1);
+
+  setTasks(tasks);
+};
+
+const getLastIndex = () => {
+  const lastIndex = LocalStorage.getItem(lastIndexKey);
+  
+  if (lastIndex) {
+    return lastIndex;
+  }
+
+  const tasks = getTasks();
+  
+  if (tasks.length > 0) {
+    return tasks[tasks.length - 1].id;
+  }
+  
+  return 0;
+};
+
+const incrementLastIndex = () => {
+  LocalStorage.setItem(lastIndexKey, getLastIndex() + 1);
+  return getLastIndex();
+};
+
+export default {
+  getTasks,
+  addTask,
+  editTask,
+  deleteTask,
+  getLastIndex,
+  incrementLastIndex
+};
